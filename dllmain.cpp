c288fb52a35545772e45962f6275c08a3077b609
sidharth kaushik <sidharth.kaushik@atos.net>
2021-04-02 15:16:38 +0530

    DLL

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,5 +1,85 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
-#include "pch.h"
+#include <Windows.h>
+#include <iostream>
+#include <TlHelp32.h>
+#include "mem.h"
+#include "proc.h"
+
+DWORD WINAPI HackThread(HMODULE hModule)
+{
+    // Create a console
+    AllocConsole();
+    FILE * f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+
+    std::cout << "Yes\n";
+
+    // Get module base
+    uintptr_t moduleBase = (uintptr_t)GetModuleHandle(L"ac_client.exe");
+
+    bool bHealth = false, bAmmo = false, bRecoil = false;
+
+    // Hack koop
+    while (true)
+    {
+        // Key input
+        if (GetAsyncKeyState(VK_END) & 1)
+        {
+            break;
+        }
+
+        if (GetAsyncKeyState(VK_NUMPAD1) & 1)
+        {
+            bHealth = !bHealth;
+        }
+
+        if (GetAsyncKeyState(VK_NUMPAD2) & 1)
+        {
+            bAmmo = !bAmmo;
+        }
+
+        if (GetAsyncKeyState(VK_NUMPAD3) & 1)
+        {
+            bRecoil = !bRecoil;
+
+            if (bRecoil)
+            {
+                mem::Nop((BYTE*)(moduleBase + 0x63786), 10);
+            }
+
+            else
+            {
+               mem::Patch((BYTE*)(moduleBase + 0x63786), (BYTE*)"\x50\x8d\x4c\x24\x1c\x51\x8b\xce\xff\xd2", 10);
+            }
+        }
+
+        // Continous write
+        uintptr_t* localPlayerPtr = (uintptr_t*)(moduleBase + 0x10f4f4);
+
+        if (localPlayerPtr)
+        {
+            if (bHealth)
+            {
+                *(int*)(*localPlayerPtr + 0xf8) = 1337;
+                // Got the localplayerptr object and then f8 offeset cast it to int pointer and then dereference it and then change the value to 1337
+            }
+
+            if (bAmmo)
+            {
+                /*uintptr_t ammoAddr = mem::FindDMAAddy(moduleBase + 0x10f4f4, { 0x374, 0x14, 0x0 });
+                int* ammo = (int*)ammoAddr;
+                *ammo = 1337;*/
+                *(int*)mem::FindDMAAddy(moduleBase + 0x10f4f4, { 0x374, 0x14, 0x0 }) = 1337;
+            }
+        }
+        Sleep(5);
+    }
+    // cleanup & eject
+    fclose(f);
+    FreeConsole();
+    FreeLibraryAndExitThread(hModule, 0);
+    return 0;
+}
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -9,6 +89,9 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+    {
+        CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr));
+    }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
---
+++ mem.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+#include "mem.h"
+
+void mem::PatchEx(BYTE* dst, BYTE* src, unsigned int size, HANDLE hProcess)
+{
+	DWORD oldprotect;
+	VirtualProtectEx(hProcess, dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
+	WriteProcessMemory(hProcess, dst, src, size, nullptr);
+	VirtualProtectEx(hProcess, dst, size, oldprotect, &oldprotect);
+}
+void mem::NopEx(BYTE* dst, unsigned int size, HANDLE hProcess)
+{
+	BYTE* nopArray = new BYTE[size];
+	memset(nopArray, 0x90, size);
+
+	PatchEx(dst, nopArray, size, hProcess);
+	delete[] nopArray;
+}
+
+uintptr_t mem::FindDMAAddy(HANDLE hproc, uintptr_t ptr, std::vector<unsigned int> offsets)
+{
+	uintptr_t addr = ptr;
+	for (unsigned int i = 0; i < offsets.size(); i++)
+	{
+		// https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-readprocessmemory
+		ReadProcessMemory(hproc, (BYTE*)addr, &addr, sizeof(addr), 0);
+		// process, baseAdd, Buffer, Size, NoOfBytesRead
+		addr += offsets[i];
+	}
+	return addr;
+}
+
+void mem::Patch(BYTE* dst, BYTE* src, unsigned int size)
+{
+	DWORD oldprotect;
+	VirtualProtect(dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
+	memcpy( dst, src, size);
+	VirtualProtect(dst, size, oldprotect, &oldprotect);
+}
+void mem::Nop(BYTE* dst, unsigned int size)
+{
+	DWORD oldprotect;
+	VirtualProtect(dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
+	memset(dst, 0x90, size); //0x90 NOP
+	VirtualProtect(dst, size, oldprotect, &oldprotect);
+}
+
+uintptr_t mem::FindDMAAddy(uintptr_t ptr, std::vector<unsigned int> offsets)
+{
+	uintptr_t addr = ptr;
+	for (unsigned int i = 0; i < offsets.size(); i++)
+	{
+		addr = *(uintptr_t*)addr;
+		addr += offsets[i];
+	}
+	return addr;
+}
